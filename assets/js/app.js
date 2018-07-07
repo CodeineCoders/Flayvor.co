4f99876a5028a1141d1d408046a362e7b4fc1b2e
Brian Racek <rac3k@aol.com>
2018-07-07 16:25:39 -0400

    username

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,7 @@ if (user != null) {
   user.providerData.forEach(function (profile) {
     console.log("  Name: " + profile.displayName);
     console.log("  Email: " + profile.email);
-    $("#userName").text("<p>" + "Welcome: " + profile.displayName + "</p>");
+    $("#userName").append("<p>" + profile.displayName + "</p>");
   });
 }
 
