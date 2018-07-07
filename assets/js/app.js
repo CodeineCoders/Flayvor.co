bd761ab32bae835700fb214ef03af230e4c1fac2
Brian <rac3k@aol.com>
2018-07-07 11:37:28 -0400

    user name display

--- assets/js/app.js
+++ assets/js/app.js
@@ -88,10 +88,11 @@ $(document).ready(function () {
 if (user != null) {
   user.providerData.forEach(function (profile) {
     console.log("Sign-in provider: " + profile.providerId);
-    console.log("  Provider-specific UID: " + profile.uid);
     console.log("  Name: " + profile.displayName);
     console.log("  Email: " + profile.email);
   });
+
+  $("#userName").text("Welcome " + profile.displayName);
 }
 
   var historySearch = {
